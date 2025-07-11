47b4c0b822c338dbe4eb6d27828158705ba961de
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-07-11 16:04:50 +0300

    rk3568: Fix formatting

--- arch/arm/boards/diasom-rk3568/lowlevel.c
+++ arch/arm/boards/diasom-rk3568/lowlevel.c
@@ -42,13 +42,13 @@ ENTRY_FUNCTION(start_rk3568_diasom_som_evb, r0, r1, r2)
 ENTRY_FUNCTION(start_rk3568_diasom_som_smarc_evb, r0, r1, r2)
 {
 	extern char __dtb_rk3568_diasom_som_smarc_evb_start[];
-	
+
 	start_rk3568_diasom(__dtb_rk3568_diasom_som_smarc_evb_start);
 }
 
 ENTRY_FUNCTION(start_rk3568_diasom_som_sodimm_evb, r0, r1, r2)
 {
 	extern char __dtb_rk3568_diasom_som_sodimm_evb_start[];
-	
+
 	start_rk3568_diasom(__dtb_rk3568_diasom_som_sodimm_evb_start);
 }

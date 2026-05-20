MacroGroup/barebox

commit b1ced2c3caa9df2c59aea1ee9fe28f6862e7570d

state: suppress state overwrite warning for barebox DT
Nowadays, the root != of_get_root_node() check always evaluates to true,
because fixups are never applied onto the barebox live tree directly,
but at a copy. This was necessary to allow repeated fixups, e.g. by
of_diff - + or with multiple boot targets.

The check was useful to have though to differentiate between the case
that the barebox DT is passed to Linux (where it's benign that the state
node is overwritten) or the Linux DT having a state node (which can
mislead as the kernel will be overwritten).

barebox already handles $properties specially in that it refuses to
create them when unflattening and silently drops them when flattening.

This allows associating data with a node for the duration it's
unflattened in barebox memory.

Make use of this by marking the barebox live tree's root node as such,
so even it's duplicates don't trigger the warning while external DTs
would.

Signed-off-by: Ahmad Fatoum <a.fatoum@barebox.org>
Link: https://lore.barebox.org/20260413100243.1456951-1-a.fatoum@barebox.org
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

drivers/of/base.c
@@ -2084,6 +2084,7 @@ int of_set_root_node(struct device_node *node)
 
 	of_chosen = of_find_node_by_path("/chosen");
 	of_property_read_string(root_node, "model", &of_model);
+	of_property_write_bool(root_node, "$barebox,root-node", true);
 
 	if (of_model)
 		barebox_set_model(of_model);

common/state/state.c
@@ -453,7 +453,7 @@ static int of_state_fixup(struct device_node *root, void *ctx)
 		 * will be overwritten. Warn about this so people do not wonder why
 		 * changes in the kernels state node do not have any effect.
 		 */
-		if (root != of_get_root_node())
+		if (!of_property_read_bool(root, "$barebox,root-node"))
 			dev_warn(&state->dev, "Warning: Kernel devicetree contains state node, replacing it\n");
 	} else {
 		char *of_path, *c;

commit 33052720e2895531df368999ba14a8d415702928

of: base: handle of_set_root_node(NULL) correctly
of_set_root_node(NULL) used to be valid and the if clause at the start
of the function explicitly allowed of it.

This was broken by the unconditional use of of_property_write_bool, so
guard that behind the existence of a root node.

Fixes: b1ced2c3caa9 ("state: suppress state overwrite warning for barebox DT")
Reported-by: Alexander Shiyan <eagle.alexander923@gmail.com>
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

drivers/of/base.c
@@ -2084,7 +2084,9 @@ int of_set_root_node(struct device_node *node)
 
 	of_chosen = of_find_node_by_path("/chosen");
 	of_property_read_string(root_node, "model", &of_model);
-	of_property_write_bool(root_node, "$barebox,root-node", true);
+
+	if (root_node)
+		of_property_write_bool(root_node, "$barebox,root-node", true);
 
 	if (of_model)
 		barebox_set_model(of_model);